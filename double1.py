1d08f3d188a4315b9abd352ccc074c883669b522
varya goryuchkina <varya0513@yandex.ru>
2020-01-08 23:53:15 +0300

    third commit

--- double1.py
+++ double1.py
@@ -1,42 +1,23 @@
+import os
+import random
 import pygame
 
 
+size = WIDTH, HEIGHT = 400, 300
+screen = pygame.display.set_mode(size)
 
-def start_screen():
-    intro_text = ["ЗАСТАВКА", "",
-                  "Для того, чтобы перейти в Правила игры, нажмите S",
-                  ""
-                  "Для того, чтобы приступить к игре, нажмите B"
-                  ""
-                  "Для того, чтобы изменить уровень, нажмите A"
-                  ]
+def load_image(name, color_key=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname).convert()
 
-    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
-    screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
-            elif event.type == pygame.K_s:
-                print('Игрок управляет длинным, тонким существом, напоминающим змею, которое ползает по плоскости '
-                      '(как правило, ограниченной стенками), собирая еду (или другие предметы), избегая столкновения '
-                      'с собственным хвостом и краями игрового поля. В некоторых вариантах на поле присутствуют '
-                      'дополнительные препятствия. Каждый раз, когда змея съедает кусок пищи, она становится длиннее, '
-                      'что постепенно усложняет игру. Игрок управляет направлением движения головы змеи (обычно 4 направления: вверх, вниз, влево, вправо), '
-                      'а хвост змеи движется следом. Игрок не может остановить движение змеи.')
 
 def load_level(filename):
     filename = "data/" + filename
@@ -65,7 +46,7 @@ tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 
 
-def generate_level1(level):
+def generate_level(level):
     new_player, x, y = None, None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
@@ -80,55 +61,112 @@ def generate_level1(level):
     return new_player, x, y
 
 
+class Camera:
+    # зададим начальный сдвиг камеры
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    # сдвинуть объект obj на смещение камеры
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    # позиционировать камеру на объекте target
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
+
+
+player, level_x, level_y = generate_level(load_level('map.txt'))
+
+FPS = 50
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+tile_images = {'wall': load_image('box.png'), 'empty': load_image('grass.png')}
+player_image = load_image('mario.png')
+
+tile_width = tile_height = 50
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
 
 
 
 class Snake(pygame.sprite.Sprite):
+
     def __init__(self, pos_x, pos_y):
         super().__init__(player_group, all_sprites)
         self.image = player_image
         self.rect = self.image.get_rect().move(tile_width * pos_x + 15, tile_height * pos_y + 5)
         self.pos = (pos_x, pos_y)
+        self.snake_body = [[100, 50], [90, 50], [80, 50]]
+        self.change_to = self.direction
 
     def move(self, x, y):
         self.pos = (x, y)
         if event.key == pygame.K_UP:
             while self.pos != ( x, 0):
-                self.rect = self.image.get_rect().move(title_width * self.pos[0], title_height * self.pos[1] + 50)
+                self.rect = self.image.get_rect().move(tile_width * self.pos[0], tile_height * self.pos[1] + 50)
         elif event.key == pygame.K_DOWN:
             while self.pos != (x, 1200):
-                self.rect = self.image.get_rect().move(title_width * self.pos[0], title_height * self.pos[1] - 50)
+                self.rect = self.image.get_rect().move(tile_width * self.pos[0], tile_height * self.pos[1] - 50)
         elif event.key == pygame.K_LEFT:
             while self.pos != (0, y):
-                self.rect = self.image.get_rect().move(title_width * self.pos[0] - 50, title_height * self.pos[1])
+                self.rect = self.image.get_rect().move(tile_width * self.pos[0] - 50, tile_height * self.pos[1])
         elif event.key == pygame.K_RIGHT:
             while self.pos != (1200, y):
-                self.rect = self.image.get_rect().move(title_width * self.pos[0] + 50, title_height * self.pos[1])
+                self.rect = self.image.get_rect().move(tile_width * self.pos[0] + 50, tile_height * self.pos[1])
 
+        def snake_body_mechanism(self, score, food_pos, screen_width, screen_height):
+            self.snake_body.insert(0, list(self.snake_head_pos))
+            if (self.snake_head_pos[0] == food_pos[0] and self.snake_head_pos[1] == food_pos[1]):
+                food_pos = [random.randrange(1, screen_width / 10) * 10, random.randrange(1, screen_height / 10) * 10]
+                score += 1
+            else:
+                self.snake_body.pop()
+                return score, food_pos
 
-
-class Balls(Snake):
-    def __init__(self):
-        super().__init__()
-        self.rad = 10
-        self.count = 0
-
-    def draw_apples(self):
-        image = load_image("apples.png")
-
-
-
-def create_block():
-    """ Создает блок в случайной позиции на карте """
-    global BLOCK
-    posx = SEG_SIZE * (random.randint(1, (WIDTH - SEG_SIZE) / SEG_SIZE))
-    posy = SEG_SIZE * (random.randint(1, (HEIGHT - SEG_SIZE) / SEG_SIZE))
-
-    # блок это кружочек красного цвета
-    BLOCK = c.create_oval(posx, posy,
-                          posx + SEG_SIZE,
-                          posy + SEG_SIZE,
-                          fill="red")
+class Food():
+    def __init__(self, food_color, screen_width, screen_height):
+        self.food_color = food_color
+        self.pos_x = 10
+        self.pos_y = 10
+        self.food_pos = [random.randrange(1, screen_width / 10) * 10, random.randrange(1, screen_height / 10) * 10]
 
 
 pygame.init()
